9e6fb09336b0ccc5ca6e98b75a1bbf7a16464303
Jonatas Vieira <jsovml@gmail.com>
2019-08-07 21:59:29 -0300

    Creating entrypoint script and changed dockerfiles to use ubuntu image.

---
+++ docker-entrypoint.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -e
+
+export HDFS_NAMENODE_USER="root"
+export HDFS_DATANODE_USER="root"
+export HDFS_SECONDARYNAMENODE_USER="root"
+export YARN_RESOURCEMANAGER_USER="root"
+export YARN_NODEMANAGER_USER="root"
+
+if [ "$1" == 'hadoop' ]; then
+    echo "Starting sshd service..."
+    /etc/init.d/ssh start
+
+    if [ "$(hostname)" == 'node-master' ]; then
+        echo "Starting hdfs.."
+        /bin/bash /opt/hadoop/sbin/start-dfs.sh
+    fi
+
+fi
+
+while true; do sleep 1000; done
