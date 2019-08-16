5a7a7a7cf87ca3a1ee465d7632dd50ada5c8b9d5
Jonatas Vieira <jsovml@gmail.com>
2019-08-16 08:53:19 -0300

    Adding hue and hiveserver.

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -6,14 +6,24 @@ export HDFS_DATANODE_USER="root"
 export HDFS_SECONDARYNAMENODE_USER="root"
 export YARN_RESOURCEMANAGER_USER="root"
 export YARN_NODEMANAGER_USER="root"
+export HADOOP_HOME="/opt/hadoop"
+export HIVE_HOME="/opt/apache-hive"
 
 if [ "$1" == 'hadoop' ]; then
     echo "Starting sshd service..."
     /etc/init.d/ssh start
 
     if [ "$(hostname)" == 'node-master' ]; then
-        echo "Starting hdfs.."
+        echo "Starting HDFS.."
         /bin/bash /opt/hadoop/sbin/start-dfs.sh
+        
+        # Create non-existing folders
+        /bin/bash /opt/hadoop/bin/hadoop fs -mkdir -p /user/hive/warehouse
+
+        /bin/bash  /opt/apache-hive/bin/init-hive-dfs.sh
+
+        echo "Starting HIVESERVER.."
+        $HIVE_HOME/bin/hive --service hiveserver2
     fi
 
 fi
---
+++ hue/init.sql
@@ -0,0 +1,5 @@
+CREATE DATABASE IF NOT EXISTS hue;
+
+-- Grant All Privileges ON *.* to 'root'@'%' Identified By 'secret'; 
+
+-- FLUSH PRIVILEGES;
