eb482d4c7687c22cc64687544ea74a66147765af
JOnatas Vieira <jsoovieira@gmail.com>
2019-10-03 14:52:09 +0000

    Fixing docker-compose to properly up services and to use volumes to persist data.

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -10,26 +10,40 @@ export YARN_NODEMANAGER_USER="root"
 export HADOOP_HOME="/opt/hadoop"
 export HIVE_HOME="/opt/apache-hive"
 
+echo "listing data directories of $(hostname)"
+/bin/ls /opt/hadoop/data
 if [ "$1" == 'hadoop' ]; then
     echo "Starting sshd service..."
     /etc/init.d/ssh start
 
     if [ "$(hostname)" == 'node-master' ]; then
+        echo "HDFS variable"
+        HDFS_ALREADY_FORMATTED=$(find "$HADOOP_HOME/data/nameNode" -mindepth 1 -print -quit 2>/dev/null)
+
+        echo "HDFS if "
+        # Checking if HDFS needs to be formated.
+        if [ !  $HDFS_ALREADY_FORMATTED ]; then
+            echo "FORMATTING NAMENODE"
+            $HADOOP_HOME/bin/hdfs namenode -format || { echo 'FORMATTING FAILED' ; exit 1; }
+
+            # tells hive to use mysql database as its metastore database.
+            $HIVE_HOME/bin/schematool -dbType mysql -initSchema  
+        fi
+
+
         echo "Starting HDFS.."
         /bin/bash /opt/hadoop/sbin/start-dfs.sh
-        
+
         # Create non-existing folders
-        /bin/bash /opt/hadoop/bin/hadoop fs -mkdir -p /user/hive/warehouse
+        $HADOOP_HOME/bin/hadoop fs -mkdir -p /user/hive/warehouse
 
         # Create hue directories
-        $HADOOP_HOME/bin/hadoop fs -mkdir /user/hue
+        $HADOOP_HOME/bin/hadoop fs -mkdir -p /user/hue
         $HADOOP_HOME/bin/hadoop fs -chown hue:hadoop /user/hue
         $HADOOP_HOME/bin/hadoop fs -chmod 755 /user/hue
 
+        echo "Initializing hive..."
         /bin/bash  /opt/apache-hive/bin/init-hive-dfs.sh
-
-        # tells hive to use derby database as its metastore database.
-        $HIVE_HOME/bin/schematool -dbType mysql -initSchema  
 
         echo "Starting HIVESERVER.."
         $HIVE_HOME/bin/hive --service hiveserver2 &
@@ -38,7 +52,10 @@ if [ "$1" == 'hadoop' ]; then
         for file in `/bin/ls /opt/hive/scripts/*.sql`; do 
            echo "Running script file named: $file"
            $HIVE_HOME/bin/hive -f "$file"
-	done
+	    done
+
+        echo "Starting YARN."
+        /bin/bash /opt/hadoop/sbin/start-yarn.sh
 
         echo "Starting webhttp hadoop service..."
         /bin/bash /opt/hadoop/bin/hdfs httpfs &
